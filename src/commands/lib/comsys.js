0f95d801812cf415f7262888fea29d4cc6d1376b
lcanady <lem.canady@gmail.com>
2019-08-24 18:38:55 -0700

    Channels

--- src/commands/lib/comsys.js
+++ src/commands/lib/comsys.js
@@ -47,12 +47,16 @@ module.exports = mush => {
     }
   });
 
+  // Command: @clist
+  // Usage: @clist [<pattern>]
+  // List the channels the enactor is eligable to
+  // join. Pattern filters the selection.
   mush.cmds.set("@clist", {
     pattern: /^@?clist\s+?(.*)/i,
     restriction: "connected",
     run: (socket, data) => {
       const enactor = mush.db.id(socket.id);
-      let [, pattern] = data;
+      let pattern = data[1];
       let output = "Channels%r%r";
       output += "%cuName%cn".padEnd(20);
       output += "%cuOwner%cn".padEnd(20);
@@ -60,7 +64,17 @@ module.exports = mush => {
       output += "%cuDescription%cn";
 
       // build the channel information string.
-      for (const channel of mush.channels.channels) {
+      channels = mush.channels.channels.filter(channel => {
+        if (
+          pattern.toLowerCase() &&
+          channel.name.toLowerCase().match(pattern, "i")
+        ) {
+          return channel;
+        } else if (!pattern) {
+          return channel;
+        }
+      });
+      for (const channel of channels) {
         if (mush.flags.hasFlags(enactor, channel.join) || !channel.join) {
           output += `%r${channel.name}`.padEnd(16);
           output += channel.owner
@@ -75,6 +89,59 @@ module.exports = mush => {
         }
       }
       mush.broadcast.send(socket, output);
+    }
+  });
+
+  // delcom
+  // USAGE: delcom <alias>
+
+  // Deletes an alias for a channel.  If it's the only
+  // alias for the channel, then the channel is removed and
+  // must be rejoined with addcom.
+  mush.cmds.set("delcom", {
+    pattern: /^delcom\s+(.*)/i,
+    restriction: "connected",
+    run: (socket, data) => {
+      const enactor = mush.db.id(socket.id);
+      const chan = find(enactor.channels, { alias: data[1] });
+      if (chan) {
+        // Get the index of the alias
+        const index = enactor.channels.indexOf(chan);
+        enactor.channels.splice(index, 1);
+        mush.db.update(enactor.id, { channels: enactor.channels });
+        mush.db.save();
+        // Send a message letting them know the alias has been removed.
+        mush.broadcast.send(
+          socket,
+          `%chDone%cn. Alias '%ch${chan.alias}%cn' removed from channel '%ch${chan.name}%cn'.`
+        );
+        const chanObj = mush.channels.get(chan.name);
+        let found = false;
+        // iterate through the channel list to see if there are any other
+        // listings for the channel available.  If no alias left, inform
+        // the channel has been removed completely.
+        for (let channel of enactor.channels) {
+          if (channel.name === chan.name) {
+            found = true;
+          }
+        }
+
+        // No matches found!
+        if (!found) {
+          mush.broadcast.send(
+            socket,
+            `You have no more aliases for channel '%ch${chan.name}%cn'.` +
+              `  To rejoin you'll have to use %chaddcom%cn again.`
+          );
+          mush.emitter.emit(
+            "channel",
+            chanObj,
+            `${enactor.name} has left this channel.`
+          );
+        }
+      } else {
+        mush.broadcast.send(socket, "I can't find that channel.");
+      }
     }
   });
 };
